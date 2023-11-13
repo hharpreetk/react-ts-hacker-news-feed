40a539f1be9b492f035a426a9346f4df1f6a9a0f
Harpreet Kaur <68283477+hharpreetk@users.noreply.github.com>
2023-11-13 05:32:14 +0000

    Add explicit types for return type

--- src/hooks/useSearchSuggestions.tsx
+++ src/hooks/useSearchSuggestions.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 
-// Custom hook for managing search suggestions in local storage
-const useSearchSuggestions = (key: string, initialState: string[]) => {
-  // Retrieve the stored suggestions from local storage using the specified key
+const useSearchSuggestions = (
+  key: string,
+  initialState: string[],
+): [string[], (newSuggestions: string[]) => void] => {
   const storedSuggestions = localStorage.getItem(key);
 
   // If storedSuggestions is not null, parse it as JSON; otherwise, use the provided initial state
@@ -10,7 +11,6 @@ const useSearchSuggestions = (key: string, initialState: string[]) => {
     ? JSON.parse(storedSuggestions)
     : initialState;
 
-  // Initialize the 'suggestions' state using the parsed suggestions or the provided initial state
   const [suggestions, setSuggestions] = useState<string[]>(initialSuggestions);
 
   // Update local storage when suggestions change
@@ -18,7 +18,6 @@ const useSearchSuggestions = (key: string, initialState: string[]) => {
     localStorage.setItem(key, JSON.stringify(suggestions));
   }, [suggestions]);
 
-  // Return current suggestions array and a function to set hew suggestions
   return [suggestions, setSuggestions];
 };
 
