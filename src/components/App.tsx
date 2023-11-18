e02c2b7468da7fe46bbd90d1488dc19d7ad6f3f5
hharpreetk <harpreetk2120@gmail.com>
2023-11-18 00:11:39 -0500

    Remove page loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import { sortBy } from "lodash";
-import { Story, Stories, StoriesState, StoriesAction } from "../types/types";
+import { Stories } from "../types/types";
 import { useSemiPersistentState } from "../hooks/useSemiPersistentState";
 import { useSearchSuggestions } from "../hooks/useSearchSuggestions";
 import { useStories, useStoriesDispatch } from "../contexts/StoriesContext";
@@ -47,10 +47,6 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
   const [url, setUrl] = useState<string>(getUrl(searchTerm, 0));
-
-  const [page, setPage] = useState<number>(0);
-
-  const [totalPages, setTotalPages] = useState<number>(0);
 
   // State to store an array of urls representing last five searches
   const [suggestions, setSuggestions] = useSearchSuggestions(
@@ -101,25 +97,17 @@ const App = () => {
     // Fetch stories
     try {
       const result = await axios.get(url);
-      const { hits, nbPages } = result.data; // Extract the hits and total pages from the response
-      if (page === 0) {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: hits,
-        });
-        // Set the total pages from API response
-        setTotalPages(nbPages);
-      } else {
-        dispatchStories({
-          type: "LOAD_MORE_STORIES",
-          payload: hits,
-        });
-      }
+      const { hits } = result.data; 
+
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: hits,
+      });
     } catch (error) {
       console.log(error);
       dispatchStories({ type: "STORIES_FETCH_FAILURE" });
     }
-  }, [url, page]);
+  }, [url]);
 
   useEffect(() => {
     handleFetchStories();
@@ -136,10 +124,7 @@ const App = () => {
     event: React.FormEvent<HTMLFormElement>
   ): void => {
     event.preventDefault();
-    // Reset the page when search term changes
-    setPage(0);
     setUrl(getUrl(searchTerm, 0));
-
     setSearchSuggestion(searchTerm);
   };
 
@@ -153,14 +138,6 @@ const App = () => {
   const { sortKey, isReverse } = sort;
 
   const sortedList = getSortedList(data, sortKey, isReverse);
-
-  // Load more results
-  const loadMoreResults = () => {
-    // Increment the page number
-    const nextPage = page + 1;
-    setPage(nextPage);
-    setUrl(getUrl(searchTerm, nextPage)); // Update the URL with the new page
-  };
 
   return (
     <div className="App">
@@ -179,21 +156,12 @@ const App = () => {
       />
       {isError ? (
         <p>Something went wrong...</p>
-      ) : isLoading && page === 0 ? (
+      ) : isLoading ? (
         <LoadingIndicator />
       ) : data.length === 0 ? (
         <NoResults />
       ) : (
-        <>
-          <StoriesList list={sortedList} />
-          {page < totalPages - 1 ? (
-            isLoading ? (
-              <LoadingMoreResults />
-            ) : (
-              <LoadMoreButton handleMore={loadMoreResults} />
-            )
-          ) : null}
-        </>
+        <StoriesList list={sortedList} />
       )}
     </div>
   );
@@ -202,11 +170,5 @@ const App = () => {
 const LoadingIndicator = () => <p>Loading...</p>;
 
 const NoResults = () => <p>No Results Found</p>;
-
-const LoadMoreButton = ({ handleMore }: { handleMore: () => void }) => (
-  <button onClick={handleMore}>Load More</button>
-);
-
-const LoadingMoreResults = () => <p>Loading More Results...</p>;
 
 export default App;
