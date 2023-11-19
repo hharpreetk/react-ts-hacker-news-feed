0ba408176e36787d9be7778102379e3449f77932
hharpreetk <harpreetk2120@gmail.com>
2023-11-19 10:49:32 -0500

    Centralize definitions of options array used in project

--- src/components/TagsFilter.tsx
+++ src/components/TagsFilter.tsx
@@ -1,27 +1,18 @@
 import Select from "react-select";
 import { MultiValueOption } from "../types/options";
+import { OPTIONS } from "../constants/options";
 
 interface TagsFilterProps {
   selectedTags: MultiValueOption;
   onTagChange: (selectedOptions: MultiValueOption) => void;
 }
 
-const options = [
-  { value: "story", label: "Stories" },
-  { value: "show_hn", label: "Show HN" },
-  { value: "ask_hn", label: "Ask HN" },
-  { value: "launch_hn", label: "Launch HN" },
-  { value: "job", label: "Jobs" },
-  { value: "poll", label: "Polls" },
-  { value: "comment", label: "Comments" },
-];
-
 const TagsFilter = ({ selectedTags, onTagChange }: TagsFilterProps) => (
   <div>
     <label>Filter by Tags:</label>
     <Select
       isMulti
-      options={options}
+      options={OPTIONS}
       value={selectedTags}
       onChange={onTagChange}
     />
---
+++ src/constants/options.ts
@@ -0,0 +1,11 @@
+const OPTIONS = [
+  { value: "story", label: "Stories" },
+  { value: "show_hn", label: "Show HN" },
+  { value: "ask_hn", label: "Ask HN" },
+  { value: "launch_hn", label: "Launch HN" },
+  { value: "job", label: "Jobs" },
+  { value: "poll", label: "Polls" },
+  { value: "comment", label: "Comments" },
+];
+
+export { OPTIONS };
