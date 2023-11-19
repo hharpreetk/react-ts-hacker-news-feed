abda2599af34a60fe3e4c6f0097084acbe4a0645
hharpreetk <harpreetk2120@gmail.com>
2023-11-19 11:40:23 -0500

    Customize display logic for selected tags

--- src/components/TagsFilter.tsx
+++ src/components/TagsFilter.tsx
@@ -1,27 +1,34 @@
 import Select from "react-select";
 import { MultiValueOption } from "../types/options";
 import { OPTIONS } from "../constants/options";
-
 interface TagsFilterProps {
   selectedTags: MultiValueOption;
   onTagChange: (selectedOptions: MultiValueOption) => void;
 }
 
-const TagsFilter = ({ selectedTags, onTagChange }: TagsFilterProps) => (
-  <div>
-    <label>Filter by Tags:</label>
-    <Select
-      isMulti
-      options={OPTIONS}
-      value={selectedTags}
-      onChange={onTagChange}
-    />
-    {selectedTags.length ? (
-      <p>Showing {selectedTags.map((tag) => tag.label).join(" or ")}</p>
-    ) : (
-      <></>
-    )}
-  </div>
-);
+const TagsFilter = ({ selectedTags, onTagChange }: TagsFilterProps) => {
+  // Customize display logic for selected tags
+  const displayTags = (tags: string[]) => {
+    return tags.length > 1
+      ? tags.slice(0, tags.length - 1).join(", ") +
+          ` or ${tags[tags.length - 1]}`
+      : tags;
+  };
+
+  return (
+    <div>
+      <label>Filter by Tags:</label>
+      <Select
+        isMulti
+        options={OPTIONS}
+        value={selectedTags}
+        onChange={onTagChange}
+      />
+      {selectedTags.length ? (
+        <p>Showing {displayTags(selectedTags.map((tag) => tag.label))}</p>
+      ) : null}
+    </div>
+  );
+};
 
 export default TagsFilter;
