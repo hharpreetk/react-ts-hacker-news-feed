16aeaff5b143de291c8cd372609418fc5b99be14
hharpreetk <harpreetk2120@gmail.com>
2023-11-19 07:33:29 -0500

    Seperate states and action types from common types

--- src/contexts/StoriesContext.tsx
+++ src/contexts/StoriesContext.tsx
@@ -1,9 +1,22 @@
 import React, { createContext, useReducer, ReactNode, useContext } from "react";
-import { Stories, StoriesState, StoriesAction } from "../types/types";
+import { Stories, Story } from "../types/types";
 
 interface StoriesContextProps {
   children: ReactNode;
 }
+
+type StoriesState = {
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+type StoriesAction =
+  | { type: "STORIES_FETCH_INIT" }
+  | { type: "STORIES_FETCH_SUCCESS"; payload: Stories }
+  | { type: "STORIES_FETCH_FAILURE" }
+  | { type: "REMOVE_STORY"; payload: Story }
+  | { type: "LOAD_MORE_STORIES"; payload: Stories };
 
 export const StoriesContext = createContext<StoriesState>(null!);
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -10,17 +10,4 @@ type Story = {
 
 type Stories = Array<Story>;
 
-type StoriesState = {
-  data: Stories;
-  isLoading: boolean;
-  isError: boolean;
-};
-
-type StoriesAction =
-  | { type: "STORIES_FETCH_INIT" }
-  | { type: "STORIES_FETCH_SUCCESS"; payload: Stories }
-  | { type: "STORIES_FETCH_FAILURE" }
-  | { type: "REMOVE_STORY"; payload: Story }
-  | { type: "LOAD_MORE_STORIES"; payload: Stories };
-
-export { Story, Stories, StoriesState, StoriesAction };
+export { Story, Stories };
