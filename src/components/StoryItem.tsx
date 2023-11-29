3292ed73494fa8f38802e2738bcbc4a756ff2e0b
hharpreetk <harpreetk2120@gmail.com>
2023-11-29 15:58:30 -0500

    Provide conditional rendering based on tag category

--- src/components/StoryItem.tsx
+++ src/components/StoryItem.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex, Anchor, Group, Badge, Text } from "@mantine/core";
+import { Card, Flex, Anchor, Group, Badge, Box, Text } from "@mantine/core";
 import { Story } from "../types/stories";
 import { format } from "timeago.js";
 
@@ -20,6 +20,44 @@ const StoryItem = ({ item, onRemoveItem }: StoryItemProps) => {
     return item._tags[0];
   };
 
+  const getContent = () => {
+    const category = getCategory();
+    switch (category) {
+      case "story":
+        return item.story_text;
+      case "job":
+        return item.job_text;
+      case "comment":
+        return item.comment_text;
+      default:
+        return null;
+    }
+  };
+
+  const getPoints = () => {
+    const category = getCategory();
+    switch (category) {
+      case "story":
+        return item.points;
+      case "poll":
+        return item.points;
+      default:
+        return null;
+    }
+  };
+
+  const getNumberOfComments = () => {
+    const category = getCategory();
+    switch (category) {
+      case "story":
+        return item.num_comments;
+      case "poll":
+        return item.num_comments;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Card withBorder radius="md">
       <Flex direction="column" gap={3}>
@@ -27,24 +65,32 @@ const StoryItem = ({ item, onRemoveItem }: StoryItemProps) => {
           <Anchor href={item.url} target="_blank" fw={500} lh="sm">
             {item.title}
           </Anchor>
-          <Text>
+          <Box>
             <Badge tt="uppercase" fw={700} size="sm" variant="light" radius={2}>
               {getCategory()}
             </Badge>
-          </Text>
+          </Box>
         </Group>
-        {item.story_text ? (
+        {getContent() && (
           <Text lineClamp={2} size="sm" c="dimmed" mb={2}>
-            {item.story_text}
+            {getContent()}
           </Text>
-        ) : null}
+        )}
         <Flex wrap="wrap" rowGap={2} columnGap="xs" align="center">
           <Text size="sm">{item.author}</Text>
           <Text size="xs">|</Text>
-          <Text size="sm">{item.points} points</Text>
-          <Text size="xs">|</Text>
-          <Text size="sm">{item.num_comments} comments</Text>
-          <Text size="xs">|</Text>
+          {getPoints() !== null && (
+            <>
+              <Text size="sm">{getPoints()} points</Text>
+              <Text size="xs">|</Text>
+            </>
+          )}
+          {getNumberOfComments() !== null && (
+            <>
+              <Text size="sm">{getNumberOfComments()} comments</Text>
+              <Text size="xs">|</Text>
+            </>
+          )}
           <Text size="sm">{getFormattedDate(item.created_at)}</Text>
           <Text size="xs">|</Text>
           <Anchor size="sm" onClick={handleRemoveItem}>
--- src/types/stories.ts
+++ src/types/stories.ts
@@ -3,6 +3,8 @@ type Story = {
   url: string;
   author: string;
   story_text: string | null;
+  job_text: string | null;
+  comment_text: string | null;
   _tags: string[];
   num_comments: number;
   points: number;
