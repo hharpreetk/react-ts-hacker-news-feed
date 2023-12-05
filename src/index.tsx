b359fe07c1d60f339585c73c44d7453fbe2c4d10
hharpreetk <harpreetk2120@gmail.com>
2023-12-05 06:25:43 -0500

    Add custom theme and styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./themes/theme";
 import App from "./components/App";
 import { StoriesProvider } from "./contexts/StoriesContext";
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <MantineProvider>
-    <StoriesProvider>
-      <App />
-    </StoriesProvider>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <StoriesProvider>
+        <App />
+      </StoriesProvider>
     </MantineProvider>
   </React.StrictMode>
 );
---
+++ src/themes/theme.ts
@@ -0,0 +1,89 @@
+import {
+  createTheme,
+  VariantColorsResolver,
+  defaultVariantColorsResolver,
+  parseThemeColor,
+  useMantineColorScheme,
+  getThemeColor,
+  useMantineTheme,
+  mergeMantineTheme,
+  DEFAULT_THEME,
+} from "@mantine/core";
+
+const variantColorResolver: VariantColorsResolver = (input) => {
+  const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
+  const defaultResolvedColors = defaultVariantColorsResolver(input);
+  const parsedColor = parseThemeColor({
+    color: input.color || input.theme.primaryColor,
+    theme: input.theme,
+  });
+
+  // Override properties of default variant
+  if (parsedColor.isThemeColor && input.variant === "default") {
+    return {
+      ...defaultResolvedColors,
+      background:
+        colorScheme === "dark"
+          ? getThemeColor("dark.4", theme)
+          : getThemeColor("gray.3", theme),
+      hover:
+        colorScheme === "dark"
+          ? getThemeColor("dark.5", theme)
+          : getThemeColor("gray.2", theme),
+      color:
+        colorScheme === "dark"
+          ? getThemeColor("gray.0", theme)
+          : getThemeColor("dark.8", theme),
+      border: "none",
+    };
+  }
+
+  // Override properties of light variant in dark color scheme
+  if (
+    parsedColor.isThemeColor &&
+    input.variant === "light" &&
+    colorScheme === "dark"
+  ) {
+    return {
+      ...defaultResolvedColors,
+      color: getThemeColor(`${parsedColor.color}`, theme),
+    };
+  }
+
+  // Override properties of transparent variant in dark color scheme
+  if (
+    parsedColor.isThemeColor &&
+    input.variant === "transparent" &&
+    colorScheme === "dark"
+  ) {
+    return {
+      ...defaultResolvedColors,
+      color: getThemeColor(`${parsedColor.color}`, theme),
+    };
+  }
+
+  // Override properties of filled variant in dark color scheme
+  if (
+    parsedColor.isThemeColor &&
+    input.variant === "filled" &&
+    colorScheme === "dark"
+  ) {
+    return {
+      ...defaultResolvedColors,
+      color: `var(--mantine-color-dark-9)`,
+    };
+  }
+
+  return defaultResolvedColors;
+};
+
+const themeOverride = createTheme({
+  primaryColor: "orange",
+  primaryShade: { light: 7, dark: 3 },
+  variantColorResolver,
+});
+
+const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
+
+export { theme };
