9844879b889f567c88b5bf07f372215a4e72a629
hharpreetk <harpreetk2120@gmail.com>
2023-12-10 08:05:36 -0500

    Remove unused imports

--- src/components/ContentFilter.tsx
+++ src/components/ContentFilter.tsx
@@ -2,7 +2,6 @@ import {
   ScrollArea,
   SegmentedControl,
   useMantineTheme,
-  useMantineColorScheme,
 } from "@mantine/core";
 import { CONTENT_OPTIONS } from "../constants/options";
 import classes from "../styles/SegmentedControl.module.css";
@@ -17,7 +16,6 @@ const ContentFilter: React.FC<ContentilterProps> = ({
   onContentChange,
 }) => {
   const theme = useMantineTheme();
-  const { colorScheme } = useMantineColorScheme();
   return (
     <ScrollArea mb={6} scrollbarSize={9} type="auto">
       <SegmentedControl
