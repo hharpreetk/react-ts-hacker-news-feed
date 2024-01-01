4feabf214dc1596b7d297cee6eecc45e02b0e48f
hharpreetk <harpreetk2120@gmail.com>
2024-01-01 14:27:09 -0500

    Show exit button on settings and story detail page

--- src/components/shared/AppHeader.tsx
+++ src/components/shared/AppHeader.tsx
@@ -2,17 +2,20 @@ import { Grid, ActionIcon } from "@mantine/core";
 import { useLocation } from "react-router-dom";
 import { IconSquareLetterH } from "@tabler/icons-react";
 import SearchBar from "../search/SearchBar";
-import ExitButton from "../shared/ExitButton";
+import ExitButton from "../settings/ExitButton";
 
 const AppHeader: React.FC = () => {
   const location = useLocation();
-  const showSearchHeader = location.pathname === "/";
+  const showSearchBar = location.pathname === "/";
+  const showExitButton = location.pathname.startsWith("/story/");
+
   return (
     <Grid px="lg" py={22} align="center" justify="space-between">
       <ActionIcon component="a" href="/" variant="transparent" size={37}>
         <IconSquareLetterH size={37} />
       </ActionIcon>
-      {showSearchHeader ? <SearchBar /> : <ExitButton />}
+      {showSearchBar && <SearchBar />}
+      {showExitButton && <ExitButton />}
     </Grid>
   );
 };
