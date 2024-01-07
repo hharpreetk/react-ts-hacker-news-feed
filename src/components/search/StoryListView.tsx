f8dea0dc1659cf62127a6c28b4ffd8d6150af6c5
hharpreetk <harpreetk2120@gmail.com>
2024-01-07 10:07:53 -0500

    Update list view

---
+++ src/components/search/StoryListView.tsx
@@ -0,0 +1,58 @@
+import { Loader, Center } from "@mantine/core";
+import StoryList from "./StoryList";
+import Pagination from "./Pagination";
+import { NO_RESULT_CONTENT_FEEDBACK } from "../../constants/feedback";
+import { useStories } from "../../contexts/StoriesContext";
+import { useSearch } from "../../contexts/SearchContext";
+import Feedback from "./Feedback";
+
+const StoryView: React.FC = () => {
+  const { data, isLoading, isError } = useStories();
+
+  return (
+    <>
+      {isError ? (
+        <ErrorFeedback />
+      ) : isLoading ? (
+        <Center py="sm">
+          <Loader type="oval" mx="auto" my="lg" />
+        </Center>
+      ) : data?.length === 0 ? (
+        <NoResultsFeedback />
+      ) : (
+        data && (
+          <>
+            <StoryList />
+            <Pagination />
+          </>
+        )
+      )}
+    </>
+  );
+};
+
+export default StoryView;
+
+const NoResultsFeedback = () => {
+  const { selectedContent } = useSearch();
+  return (
+    <Feedback
+      status="info"
+      message={`No '${NO_RESULT_CONTENT_FEEDBACK[selectedContent || 'stories']}' were found matching your search.`}
+    />
+  );
+};
+
+const ErrorFeedback = () => {
+  const { error } = useStories();
+  return (
+    <Feedback
+      status="error"
+      message={
+        error
+          ? `Error ${error.code}: ${error?.message}`
+          : "Oops! Something went wrong."
+      }
+    />
+  );
+};
