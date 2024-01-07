4ca35d47629a52ba3884635ea366da34bf4c20ef
hharpreetk <harpreetk2120@gmail.com>
2024-01-07 10:15:12 -0500

    Update story feedback ui

--- src/components/story/StoryView.tsx
+++ src/components/story/StoryView.tsx
@@ -1,15 +1,23 @@
-import { Text } from "@mantine/core";
+import { Center, Loader, Text } from "@mantine/core";
 import { useFetchStory } from "../../hooks/useFetchStory";
 import StoryDetail from "./StoryDetail";
 import { useParams } from "react-router-dom";
+import Feedback from "../search/Feedback";
 
 const StoryView: React.FC = () => {
   const { id } = useParams();
   const { data, isLoading, error } = useFetchStory(id);
   return error ? (
-    <Text>Error fetching comments: {error.message}</Text>
+    <Feedback
+      status="error"
+      message={
+        error ? `Error : ${error?.message}` : "Oops! Something went wrong."
+      }
+    />
   ) : isLoading ? (
-    <Text>Loading story...</Text>
+    <Center py="sm" my="lg">
+      <Loader type="oval" mx="auto" />
+    </Center>
   ) : (
     <StoryDetail story={data} />
   );
