8ed58a2a082225692f6d7207af07d270f9c2d6c0
hharpreetk <harpreetk2120@gmail.com>
2024-01-07 03:25:01 -0500

    Fallback to stories in feedback if the selectedContent is null

--- src/components/search/StoryView.tsx
+++ src/components/search/StoryView.tsx
@@ -38,7 +38,7 @@ const NoResultsFeedback = () => {
   return (
     <Feedback
       status="info"
-      message={`No '${NO_RESULT_CONTENT_FEEDBACK[selectedContent]}' were found matching your search.`}
+      message={`No '${NO_RESULT_CONTENT_FEEDBACK[selectedContent || 'stories']}' were found matching your search.`}
     />
   );
 };
