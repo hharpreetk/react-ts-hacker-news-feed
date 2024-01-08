56292ab692c49710beef5ae15c611664f47e72e0
hharpreetk <harpreetk2120@gmail.com>
2024-01-08 12:44:53 -0500

    Add types for story

---
+++ src/types/story.ts
@@ -0,0 +1,19 @@
+type Story = {
+  title: string;
+  url: string;
+  author: string;
+  text: string | null;
+  points: number;
+  created_at: string;
+  children: Array<Comment>;
+};
+
+type Comment = {
+  id: string;
+  author: string;
+  created_at: string;
+  children: Array<Comment>;
+  text: string;
+};
+
+export { Story, Comment };
