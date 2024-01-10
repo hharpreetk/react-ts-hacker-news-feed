55d8c81ff3f73fad420b1df258e447fd8c40c53f
hharpreetk <harpreetk2120@gmail.com>
2024-01-10 12:18:05 -0500

    Update skeleton ui

--- src/components/story/StorySkeleton.tsx
+++ src/components/story/StorySkeleton.tsx
@@ -4,20 +4,20 @@ import classes from "../../styles/Skeleton.module.css";
 const StorySkeleton: React.FC = () => {
   const renderSkeletonLines = (length: number) => {
     return Array.from({ length }).map((_, index) => (
-      <Skeleton key={index} height={15} mb={8} />
+      <Skeleton key={index} height={16} mb={8} />
     ));
   };
 
   const renderSkeletonText = (length: number) => {
     return Array.from({ length }).map((_, index) => (
-      <Skeleton key={index} width={50} height={15} />
+      <Skeleton key={index} width={50} height={16} />
     ));
   };
 
   return (
     <Flex direction="column" gap={1}>
       {renderSkeletonLines(2)}
-      <Flex wrap="wrap" rowGap={3} columnGap="xs" align="center" mt={1} mb="sm">
+      <Flex wrap="wrap" rowGap={3} columnGap="xs" align="center" mt={1} mb="xs">
         {renderSkeletonText(4)}
       </Flex>
       <Skeleton classNames={{ root: classes.storyContent }} />
