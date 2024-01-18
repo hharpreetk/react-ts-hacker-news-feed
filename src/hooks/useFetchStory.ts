136a1384ec130cf4baa0e3bc338bcfaa8dc2684f
hharpreetk <harpreetk2120@gmail.com>
2024-01-18 12:05:13 -0500

    Remove try catch block

--- src/hooks/useFetchStory.ts
+++ src/hooks/useFetchStory.ts
@@ -5,33 +5,27 @@ import { getStoryUrl } from "../api/api";
 const useFetchStory = (storyId: string | undefined) => {
   const url = getStoryUrl(storyId || "");
 
-  const { data, isPending, isLoading, isError, error } = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: [storyId],
     queryFn: async () => {
-      try {
-        const response = await fetch(url);
+      const response = await fetch(url);
 
-        if (!response.ok) {
-          let errorMessage = `Status ${response.status} error.`;
+      if (!response.ok) {
+        let errorMessage = `Status ${response.status} error.`;
 
-          // Customize error message based on different status codes
-          if (response.status === 404) {
-            errorMessage = "Story not found.";
-          } else if (response.status === 500) {
-            errorMessage = "Internal server error.";
-          }
-
-          throw new Error(errorMessage);
+        // Customize error message based on different status codes
+        if (response.status === 404) {
+          errorMessage = "Story not found.";
+        } else if (response.status === 500) {
+          errorMessage = "Internal server error.";
         }
 
-        return response.json();
-      } catch (error) {
-        //Rethrow error to be caught by the query
-        throw error;
+        return new Error(errorMessage);
       }
+      return response.json();
     },
   });
-  return { data, isLoading, isPending, isError, error };
+  return { data, isPending, isError, error };
 };
 
 export { useFetchStory };
