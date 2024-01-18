8d037364a666cc71dce500ffbbe96a22d1273aa9
hharpreetk <harpreetk2120@gmail.com>
2024-01-18 06:48:37 -0500

    Improve error handling in react query

--- src/hooks/useFetchStory.ts
+++ src/hooks/useFetchStory.ts
@@ -5,15 +5,29 @@ import { getStoryUrl } from "../api/api";
 const useFetchStory = (storyId: string | undefined) => {
   const url = getStoryUrl(storyId || "");
 
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, isPending, isError, error } = useQuery({
     queryKey: [storyId],
     queryFn: async () => {
-      const result = await axios.get(url);
-      return result.data;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        let errorMessage = `Status ${response.status} error.`;
+
+        // Customize error message based on different status codes
+        if (response.status === 404) {
+          errorMessage = "Story not found.";
+        } else if (response.status === 500) {
+          errorMessage = "Internal server error.";
+        }
+
+        throw new Error(errorMessage);
+      }
+
+      return response.json();
     },
     throwOnError: false,
   });
-  return { data, isLoading, error };
+  return { data, isLoading, isPending, isError, error };
 };
 
 export { useFetchStory };
