9ac191fa91fc81da96f592b323c21f340c33efa4
hharpreetk <harpreetk2120@gmail.com>
2024-01-20 09:55:02 -0500

    Update spacing in replies ui

--- src/components/story/StoryComment.tsx
+++ src/components/story/StoryComment.tsx
@@ -65,7 +65,7 @@ const StoryComment = ({ comment }: { comment: Comment }) => {
           <Button
             variant="transparent"
             fz={15}
-            style={{ padding: 0, marginBottom: rem(-14), marginTop: rem(-10) }}
+            style={{ padding: 0, marginBottom: rem(-12), marginTop: rem(-12) }}
             onClick={() => setShowReplies(!showReplies)}
           >
             {showReplies ? (
